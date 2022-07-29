65b6a62f5f34da3da880c602daedc73e15bdaa7a
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-29 17:13:57 +0530

    smart contract done

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React,{useContext} from 'react'
+import logo from '../assets/amazon_logo_full.png'
+import Image from 'next/image'
+import { IoMdSearch } from 'react-icons/io'
+import { AmazonContext } from '../context/AmazonContext'
+import { FaCoins } from 'react-icons/fa'
+import { CgMenuGridO } from 'react-icons/cg'
+const Header = () => {
+  return (
+    <div>
+        Header
+    </div>
+  )
+}
+
+export default Header
