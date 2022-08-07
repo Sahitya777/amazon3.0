b16160fe5514a78ff7fe7abe8be594bb59ab8b26
Sahitya77 <sahityanijhawan@gmail.com>
2022-08-07 19:56:25 +0530

    ..

--- context/AmazonContext.js
+++ context/AmazonContext.js
@@ -1,12 +1,19 @@
 import { createContext,useState,useEffect } from "react";
 import {useMoralis, useMoralisQuery} from 'react-moralis'
-
+import { amazonAbi,amazonCoinAddress } from "../lib/constants";
+import { ethers } from "ethers";
 export const AmazonContext=createContext()
 
 export const AmazonProvider =({children})=>{
     const [username,setUsername]=useState('')
     const [nickname, setnickname] = useState('')
     const [assets,setAssets]=useState([])
+    const [balance, setBalance] = useState('')
+    const [tokenAmount, setTokenAmount] = useState('')
+    const [amountDue, setAmountDue] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [etherscanLink, setEtherscanLink] = useState('')
+    const [currentAccount, setCurrentAccount] = useState('')
 
     const {
         authenticate,
@@ -42,10 +49,13 @@ export const AmazonProvider =({children})=>{
         await enableWeb3()
       }
       if(isAuthenticated){
+          await getBalance()
           const currentUsername = await user?.get('nickname')
           setUsername(currentUsername)
+          const account=await user?.get('ethAddress')
+          setCurrentAccount(account)
       }
-    },[isWeb3Enabled,isAuthenticated,user,username])
+    },[isWeb3Enabled,isAuthenticated,user,username,currentAccount])
 
 
 
@@ -63,6 +73,56 @@ export const AmazonProvider =({children})=>{
         }
       }
 
+      const getBalance=async()=>{
+        try{
+          if(!isAuthenticated || currentAccount){
+            return;
+          }
+          const options={
+            contractAddress:amazonCoinAddress,
+            functionName:'balanceOf',
+            abi:amazonAbi,
+            params:{
+              account:currentAccount
+            },
+
+
+          }
+          if(isWeb3Enabled){
+            const response=await Moralis.executeFunction(options)
+            setBalance(response.toString())
+          }
+        }catch(error){
+          console.log(error);
+        }
+      }
+
+      const buyTokens=async()=>{
+        if(!isAuthenticated){
+          await authenticate()
+        }
+        const amount=ethers.BigNumber.from(tokenAmount);
+        const price = ethers.BigNumber.from('100000000000000')
+        const calcPrice = amount.mul(price)
+
+        let options = {
+          contractAddress: amazonCoinAddress,
+          functionName: 'mint',
+          abi: amazonAbi,
+          msgValue: calcPrice,
+          params: {
+            amount,
+          },
+        }
+      const transaction = await Moralis.executeFunction(options)
+      const receipt = await transaction.wait(4)
+      setIsLoading(false)
+      console.log(receipt)
+      setEtherscanLink(
+        `https://rinkeby.etherscan.io/tx/${receipt.transactionHash}`,
+      )
+      }
+
 
 
     return(
@@ -74,7 +134,18 @@ export const AmazonProvider =({children})=>{
                 username,
                 setUsername,
                 handleSetUsername,
-                assets
+                assets,
+                balance,
+                tokenAmount,
+                setTokenAmount,
+                amountDue,
+                setAmountDue,
+                setEtherscanLink,
+                etherscanLink,
+                currentAccount,
+                isLoading,
+                setIsLoading,
+                buyTokens
             }}
         >
             {children}
