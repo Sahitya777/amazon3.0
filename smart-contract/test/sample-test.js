c392d9effa0751d73da6d38169fb898cbb7a3249
Sahitya77 <sahityanijhawan@gmail.com>
2022-08-08 13:28:22 +0530

    smart contract deployed and connected

--- smart-contract/test/sample-test.js
+++ smart-contract/test/sample-test.js
@@ -3,8 +3,8 @@ const { ethers } = require("hardhat");
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const AmazonCoin = await ethers.getContractFactory("AmazonCoin");
+    const greeter = await AmazonCoin.deploy("Hello, world!");
     await greeter.deployed();
 
     expect(await greeter.greet()).to.equal("Hello, world!");
