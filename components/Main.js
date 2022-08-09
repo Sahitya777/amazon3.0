3a306b159d4d4808d3853636a59b8f4eff788568
Sahitya77 <sahityanijhawan@gmail.com>
2022-08-09 12:13:30 +0530

    ..

--- components/Main.js
+++ components/Main.js
@@ -2,17 +2,47 @@ import React ,{useContext} from 'react'
 import { AmazonContext } from '../context/AmazonContext'
 import Cards from './Cards'
 import Header from './Header'
+import Featured from './Featured'
 const styles={
     container:`h-full w-full flex flex-col mt-[50px] pr-[50px] overflow-hidden`,
     recentTitle: `text-2xl font-bold text-center mb-[20px] text-center mt-[40px]`,
     recentTransactionsList: `flex flex-col`,
     transactionCard: `flex justify-between mb-[20px] p-[30px] bg-[#42667e] text-white rounded-xl shadow-xl font-bold gap-[20px] text-xl`,
 }
+
 const Main = () => {
+  const {recentTransactions}=useContext(AmazonContext)
   return (
     <div className={styles.container}>
         <Header/>
+        <Featured/>
         <Cards/>
+        {recentTransactions.length>0 &&(
+          <h1 className={styles.recentTitle}>Recent Transaction</h1>
+        )}
+        {recentTransactions && recentTransactions.map((transaction,index)=>{
+          return(
+            <div className={styles.recentTransactionsList} key={index}>
+              <div className={styles.transactionCard}>
+              <p>From: {transaction.attributes.from_address}</p>
+                <p>To: {transaction.attributes.to_address} </p>
+                <p>
+                  Hash:{' '}
+                  <a
+                    target={'_blank'}
+                    rel='noopener noreferrer'
+                    href={`https://rinkeby.etherscan.io/tx/${transaction.attributes.hash}`}
+                  >
+                    {transaction.attributes.hash.slice(0, 10)}
+                  </a>
+                </p>
+                <p>Gas: {transaction.attributes.gas}</p>
+              </div>
+            </div>
+          )
+        })
+
+        }
     </div>
   )
 }
